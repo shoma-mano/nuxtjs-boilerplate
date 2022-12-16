d46c688489adfe1b4340cb66e75301e68cf38490
真野笑馬 <ms2geki@gmail.com>
2022-12-16 13:38:56 +0900

    test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 export default defineNuxtConfig( {
   pages: true,
   routeRules: {
-    '/index':{static:true},
+    '/**':{static:true},
     '/posts/1':{static:true},
     '/posts/2':{static:true},
     '/posts/**': {static:true},
